e642966718328d71684a81c3ea97e003f4ce397f
NabiilahDatabase <49357045+NabiilahDatabase@users.noreply.github.com>
2019-05-03 14:30:42 +0700

    Update buatInvoice.js

--- buatInvoice.js
+++ buatInvoice.js
@@ -18,7 +18,7 @@ function toDuit(xxx) {
 
 function buatInvoice(data) {
   var invoicePrint = '';
-
+message(data.length);
  for (var z in data) { //inv
   var id = data[z][0];
   var penerima = data[z][1];
