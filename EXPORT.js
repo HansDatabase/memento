7576c40ab40b78faa6bff38c3f9bde04339d5a44
NabiilahDatabase <49357045+NabiilahDatabase@users.noreply.github.com>
2019-05-03 14:46:10 +0700

    Update EXPORT.js

--- EXPORT.js
+++ EXPORT.js
@@ -56,8 +56,8 @@ function buatInvoice(data) {
 	'</tr>'+'\n';
   }; //brg
 
-invoicePrint += 'testing ';
-/*
+invoicePrint += 
+
 '<table width="100%" border="0" cellspacing="0" class="print-data printLabel" id="'+(z+1)+'">'+'\n'+
 '	<tr><td width="20%" rowspan="3" style="display: none" class="text-center shop-logo">'+'\n'+
 '			<img class="img-logo" src="https://image.flaticon.com/icons/svg/33/33658.svg" width="120">'+'\n'+
@@ -435,7 +435,7 @@ invoicePrint += 'testing ';
 '		</td>'+'\n'+
 '	</tr>'+'\n'+
 '</table>\n';
-*/
+
  }; //inv
 
 var template =
