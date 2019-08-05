a67946e2db2843e62b608f93adc5b78058999e94
NabiilahDatabase <49357045+NabiilahDatabase@users.noreply.github.com>
2019-08-05 22:00:30 +0700

    cek telat

--- dataKaryawan.js
+++ dataKaryawan.js
@@ -41,3 +41,20 @@ var belum = true;
   }
   if (belum) {message(cs+" belum absen masuk")}
 }
+
+//Cek telat apa gak ya
+function cekTelat(wkt) {
+  var batas = [9,5];//jam 9 lebih 5 menit
+  var terlambat = (batas[0]*60)+batas[1];
+  var masuk = (moment(wkt).format("HH")*60)+(moment(wkt).format("mm")*1);
+  var stat = new Object();
+
+  if (masuk>terlambat) {
+    stat.status="Terlambat";
+    stat.pesan="Wah telat nih";
+  } else {
+    stat.status="Tepat Waktu";
+    stat.pesan="Joss joss semangat!! :D";
+  }
+  return stat;
+}
