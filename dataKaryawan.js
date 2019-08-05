5923bb5b957ac5842f825ee4598ad6d3c67b760b
NabiilahDatabase <49357045+NabiilahDatabase@users.noreply.github.com>
2019-08-05 22:41:28 +0700

    cektelat new

--- dataKaryawan.js
+++ dataKaryawan.js
@@ -43,18 +43,31 @@ var belum = true;
 }
 
 //Cek telat apa gak ya
-function cekTelat(wkt) {
+function cekTelat(data) {
+  var nama = decrypt(data.split(":")[1]);
+  var waktu = moment.unix(data.split(":")[0]/1000).toDate().getTime();
+
+  validasi(nama);
+
   var batas = [9,5];//jam 9 lebih 5 menit
   var terlambat = (batas[0]*60)+batas[1];
-  var masuk = (moment(wkt).format("HH")*60)+(moment(wkt).format("mm")*1);
-  var stat = new Object();
+  var masuk = (moment(waktu).format("HH")*60)+(moment(waktu).format("mm")*1);
+  var stat="",msg="";
 
   if (masuk>terlambat) {
-    stat.stat="Terlambat";
-    stat.msg="Wah telat nih";
+    stat="Terlambat";
+    msg="Wah telat nih";
   } else {
-    stat.stat="Tepat Waktu";
-    stat.msg="Joss joss semangat!! :D";
+    stat="Tepat Waktu";
+    msg="Joss joss semangat!! :D";
   }
-  return stat;
+
+  var absen = new Object();
+  absen["Nama"] = nama;
+  absen["Waktu Masuk"] = waktu;
+  absen["Jam Masuk"] = waktu;
+  absen["Status"] = stat;
+  lib().create(absen);
+
+  message(msg);
 }
